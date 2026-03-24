6a3c8e1853848c4b8c6291279505404dc6434345
Spim <115038993+ognjen28a@users.noreply.github.com>
2026-03-24 17:35:14 +0100

    Initialize Flask app with basic route
    Set up a basic Flask application with CORS support.

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 import os
-djkflsdkj
+import sqlite3
+from flask import Flask, request, jsonify, json
+from flask-cors import CORS
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return "Zdravo"
+
+
+if __name__ == '__main__':
+  app.run()
