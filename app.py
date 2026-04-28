cd03df89a0c56ab397d70e8b953a2a6f71efdfc9
Spim <115038993+ognjen28a@users.noreply.github.com>
2026-04-28 11:07:08 +0200

    Add restaurant listing and update routes

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
 import os
 import sqlite3
-from flask import Flask, request, jsonify, json
+from flask import Flask, request, jsonify, json, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return "Zdravo"
+  nazivSpiska = "Spisak restorana"
+  spisakRestorana = ["Pastica","Pica Tim","HasHub", "Sahara"]
+
+  return render_template("index.html",naziv=nazivSpiska, spisak = spisakRestorana)
+
+@app.route("/restoran")
+def restoran():
+  nazivRestorana = "Pastica"
+  spisakJela = ["Karbonara mala","Karbonara velika","Srednja", "nzm"]
+
+  return render_template("restoran.html",naziv=nazivRestorana, spisak = spisakJela)
+
+if __name__ == '__main__':
+  app.run()
 
 @app.route("/primer-string")
 def string():
-  return "Tekst"
+  return "ovo be mno dugacak testk"
+
+@app.route("/primer-broj")
+def broj():
+  return 265
 
 @app.route("/primer-niz")
 def niz():
@@ -21,27 +38,27 @@ def niz():
 @app.route("/primer-json")
 def primerJson():
   data = {
-    "message": "JSON response",
-    "status" : "success"
+    "message": "This is a JSON response",
+    "status": "success"
   }
   return (data)
 
-@app.route("/primer-html")
+@app.route ("/primer-html")
 def primerHTML():
   data = """<!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset = "UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-  <link rel="stylesheet" href="static/style.css">
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width,
+initial-scale=1.0">
+   <meta http-equiv= "X-UA-Compatible" content="ie=edge">
+   <title>Document</title>
+   <link rel="stylesheet" href="static/style.css">
 </head>
 <body>
-  <h1>Zdravo programeri</h1>
+   <h1>Zdravo brt</h1>
 </body>
 </html>"""
   return data
-
-if __name__ == '__main__':
-  app.run()
+  if __name__ == '__main__':
+    app.rn()
